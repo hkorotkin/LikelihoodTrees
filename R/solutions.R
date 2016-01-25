39809d7de121db1c1c0f16ded21cd9754cc2d90f
Brian O'Meara <omeara.brian@gmail.com>
2016-01-25 00:32:03 -0500

    solutions done

--- R/solutions.R
+++ R/solutions.R
@@ -11,3 +11,17 @@ InferMorphologyTree_solution <- function(input.path = "inst/extdata/", input.fil
 	return(list(parsimony.tree=parsimony.tree, ml.tree=ml.tree))
 }
 
+InferDNATreeWithBootstrappingAndPartitions_solution <- function (input.path = "inst/extdata/", input.file = "dna.phy", input.partition = "dna12_3.partition.txt", output.path="~/Desktop/", output.name = "dna1", random.seed=12345, boot.seed=12345, model="GTRGAMMA", boot=100) {
+	system(paste("cp ", input.path, input.file, " ", output.path,input.file, sep=""))
+	system(paste("cp ", input.path, input.partition, " ", output.path,input.partition, sep=""))
+	cur.wd <- getwd()
+	setwd(output.path)
+	raxml.call <- paste("raxmlHPC -f a  -k -m ", model, " -p ", random.seed, " -x ", boot.seed, " -q ", input.partition, " -# ", boot, " -s ", input.file, " -n ", output.name, sep="")
+	status <- system(raxml.call)
+	
+	ml.tree <- read.tree(paste("RAxML_bestTree.",output.name, sep=""))
+	ml.with.bs.tree <- read.tree(paste("RAxML_bipartitions.",output.name, sep=""))
+	bs.trees <- read.tree(paste("RAxML_bootstrap.",output.name, sep=""))
+	setwd(cur.wd)
+	return(list( ml.tree=ml.tree, ml.with.bs.tree=ml.with.bs.tree, bs.trees))
+}
--- tests/testthat/test_solution.R
+++ tests/testthat/test_solution.R
@@ -1,8 +1,15 @@
 test_that("InferMorphologyTree_solution",{
-	results <- InferMorphologyTree_solution()
+	results <- InferMorphologyTree_solution(output.path=temp.dir())
 	expect_is(results$parsimony.tree, "phylo")
 	expect_is(results$ml.tree, "phylo")
 	expect_true(is.null(results$parsimony.tree$edge.length))
 	expect_false(is.null(results$ml.tree$edge.length))
+})
+
+test_that("InferDNATreeWithBootstrappingAndPartitions_solution",{
+	results <- InferDNATreeWithBootstrappingAndPartitions_solution(output.path=temp.dir())
+	expect_is(results$ml.tree, "phylo")
+	expect_false(is.null(results$ml.with.bs.tree$node.label))
+	expect_equals(length(results[[3]]), 100)
 })
 
